b8fc086df5e8267e785919b1106ace3ade887174
estrattonbailey <ericstrattonbailey@gmail.com>
2017-09-10 14:13:40 -0400

    Add log

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import io from 'socket.io-client'
 
 window.slater = window.slater || {}
 
-window.slater.reload = function (href = 'https://localhost:3000', cb) {
+window.slater.reload = function (href = 'https://localhost:3000/', cb) {
   const socket = io(href)
 
   __webpack_public_path__ = href // eslint-disable-line camelcase, no-undef
 
   socket.on('refresh', data => cb ? cb(data) : window.location.reload())
+
+  console.log(`@slater/reload active on ${href}`)
 }
