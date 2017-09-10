471b193288d66d1d36f3debf9a2425cc6c026a12
estrattonbailey <ericstrattonbailey@gmail.com>
2017-09-10 14:10:59 -0400

    Use default param val

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import io from 'socket.io-client'
 
 window.slater = window.slater || {}
 
-window.slater.reload = function (href, cb) {
-  const socket = io(href || 'https://localhost:3000')
+window.slater.reload = function (href = 'https://localhost:3000', cb) {
+  const socket = io(href)
 
-  __webpack_public_path__ = href || 'https://localhost:3000' // eslint-disable-line camelcase, no-undef
+  __webpack_public_path__ = href // eslint-disable-line camelcase, no-undef
 
   socket.on('refresh', data => cb ? cb(data) : window.location.reload())
 }
